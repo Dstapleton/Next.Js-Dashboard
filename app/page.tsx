886e7147b225b26ac74c7ebad5dc04160ead61c8
DavidS <dstapletongo@gmail.com>
2026-03-07 22:50:52 -0500

    Added loading status to images and got them to display correct diffrent devices

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import { ephesis } from "./ui/fonts";
 import Image from "next/image";
+import IsMobile from "./ui/hooks/getDevice";
 
 export default function Page() {
   return (
@@ -34,17 +35,19 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
+            loading="eager"
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className={styles.desktopImage}
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
+            loading="eager"
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className={styles.mobileImage}
+            className="block md:hidden"
             alt="Screenshot of the dashboard project showing mobile version"
           />
         </div>
--- app/ui/hooks/getDevice.tsx
+++ app/ui/hooks/getDevice.tsx
@@ -1,2 +1,12 @@
+"use client";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 
-
+//
+export default function IsMobile() {
+  const request = new NextRequest("http://localhost:3000");
+  const url = request.nextUrl;
+  const { device } = userAgent(request);
+  const viewport = device.type || "desktop";
+  url.searchParams.set("viewport", viewport);
+  console.log(`Viewport: ${viewport}`);
+}
