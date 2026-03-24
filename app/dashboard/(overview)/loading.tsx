0dc6b7e6471f8946b503868cee17a2b1a78e6c9f
DavidS <dstapletongo@gmail.com>
2026-03-24 18:19:14 -0400

    add override parth

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,5 @@
-import DashBoardSkeleletons from "@/app/ui/skeletons";
+import DashBoardSkeleletons from '@/app/ui/skeletons'
+
 export default function Loading() {
-  return <DashBoardSkeleletons />;
+  return <DashBoardSkeleletons />
 }
